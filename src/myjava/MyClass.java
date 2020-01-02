718a7a148c7c15dd39b3c53310d3f4ea661cb897
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-02 14:38:16 +0900

    first commit

---
+++ src/myjava/MyClass.java
@@ -0,0 +1,13 @@
+package myjava;
+
+public class MyClass {
+	public static void main(String[] args) {
+		// 클래스명과 파일명이 같다.
+		// main 메소드는 하나 있어야 한다.
+		/*
+		System.out.println("내일은 뭐 먹지?");
+		System.out.println("내일은 햄버거");
+		*/
+		System.out.println("모레는 피자");
+	}
+}
