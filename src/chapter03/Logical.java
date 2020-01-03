70e3172e9a52b14027b3c2a65c3007db56670fab
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-03 11:05:49 +0900

    logical

---
+++ src/chapter03/Logical.java
@@ -0,0 +1,37 @@
+package chapter03;
+
+public class Logical {
+	public static void main(String[] args) {
+		// && || !
+		// 피연산자의 타입은 boolean
+		// 결과도 boolean
+		
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(false && false);
+		
+		System.out.println();
+		
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+		
+		System.out.println();
+		
+		System.out.println(!true);
+		System.out.println(!false);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
