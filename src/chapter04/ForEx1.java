f1fa498a22570dcd81713bf874eab52b7a92380a
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-03 15:12:46 +0900

    for, 구구단

---
+++ src/chapter04/ForEx1.java
@@ -0,0 +1,20 @@
+package chapter04;
+
+public class ForEx1 {
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println(i);
+		}
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.println("3 X " + i + " = " + (3 * i));
+		}
+	}
+}
+
+
+
+
+
+
---
+++ src/chapter04/ForEx2.java
@@ -0,0 +1,17 @@
+package chapter04;
+
+import java.util.Scanner;
+
+public class ForEx2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("몇단? :");
+		String d = scanner.nextLine();
+		int num = Integer.parseInt(d);
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(num + " X " + i + " = " + (num * i));
+		}
+	}
+}
