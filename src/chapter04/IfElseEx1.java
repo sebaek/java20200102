86a3fc6867e4f2392bd719501e0bcb74fff23c42
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-03 12:28:40 +0900

    if

---
+++ src/chapter04/IfElseEx1.java
@@ -0,0 +1,23 @@
+package chapter04;
+
+import java.util.Scanner;
+
+public class IfElseEx1 {
+	public static void main(String[] args) {
+		int intVar1 = 30;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("나이는 :");
+		String input = scanner.nextLine();
+		
+		intVar1 = Integer.parseInt(input);
+		
+		System.out.println(input);
+		
+		if (intVar1 > 20) {
+			System.out.println("어른입니다.");
+		}
+		
+	}
+}
