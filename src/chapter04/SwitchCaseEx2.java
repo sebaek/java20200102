bdc3f9062ffce381d61b5bc2a234c7244ebd8dc3
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-03 14:40:44 +0900

    switch case

--- src/chapter04/SwitchCaseEx2.java
+++ src/chapter04/SwitchCaseEx2.java
@@ -11,18 +11,23 @@ public class SwitchCaseEx2 {
 
 		switch (level) {
 		case "A":
+		case "a":
 			System.out.println("90점 이상입니다.");
 			break;
 		case "B":
+		case "b":
 			System.out.println("80점 이상입니다.");
 			break;
 		case "C":
+		case "c":
 			System.out.println("70점 이상입니다.");
 			break;
 		case "D":
+		case "d":
 			System.out.println("60점 이상입니다.");
 			break;
 		case "F":
+		case "f":
 			System.out.println("60점 미만입니다.");
 			break;
 		default:
