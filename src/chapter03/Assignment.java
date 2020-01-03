46d7fc03551029c25b6c4110f9e98d8ec80e95cb
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-03 10:24:37 +0900

    assignment operator

---
+++ src/chapter03/Assignment.java
@@ -0,0 +1,26 @@
+package chapter03;
+
+public class Assignment {
+	public static void main(String[] args) {
+		// = += -= *= /= %=
+
+		int intVar1 = 50;
+		intVar1 += 70;
+		System.out.println(intVar1);
+		
+		intVar1 -= 80;
+		System.out.println(intVar1);
+		
+		intVar1 /= 3;
+		System.out.println(intVar1);
+		
+		intVar1 %= 5;
+		System.out.println(intVar1);
+	}
+}
+
+
+
+
+
+
