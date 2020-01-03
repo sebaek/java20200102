d0d4b54dce0dc4d70dc83280a670d4f19953e3fb
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-03 09:40:11 +0900

    arithmetic operator

---
+++ src/chapter03/Arithmetic.java
@@ -0,0 +1,21 @@
+package chapter03;
+
+public class Arithmetic {
+	public static void main(String[] args) {
+		// + - * / % ++ --
+		int intVar1 = 10;
+		int intVar2 = 30;
+		System.out.println(intVar1 + intVar2);
+		System.out.println(intVar1 - intVar2);
+		System.out.println(intVar1 * intVar2);
+		System.out.println(intVar1 / intVar2);
+		System.out.println(intVar1 % intVar2);
+		intVar1++;
+		System.out.println(intVar1);
+		intVar2--;
+		System.out.println(intVar2);
+		
+	}
+}
+
+
