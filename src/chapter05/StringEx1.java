73b38707d8a94f83750ffec0f2625e771ed8819c
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-03 12:10:35 +0900

    string

--- src/chapter05/StringEx1.java
+++ src/chapter05/StringEx1.java
@@ -14,6 +14,16 @@ public class StringEx1 {
 		System.out.println(strVar1.trim());
 		
 		System.out.println(strVar1.substring(4, 7));
+
+		String strVar2 = "유린기";
+		String strVar3 = strVar1 + strVar2;
+		System.out.println(strVar3);
+		
+		String strVar4 = strVar2 + 99;
+		System.out.println(strVar4);
+		
+//		String strVar5 = "3" - "4"; // 안됨.
+		
 		
 		
 		
---
+++ src/chapter05/StringEx2.java
@@ -0,0 +1,13 @@
+package chapter05;
+
+public class StringEx2 {
+	public static void main(String[] args) {
+		// String을 다른 타입으로 변환
+		String strVar1 = "9876";
+		int intVar1 = Integer.parseInt(strVar1);
+		
+		String strVar2 = "3.14";
+		double doubleVar1 = Double.parseDouble(strVar2);
+		
+	}
+}
