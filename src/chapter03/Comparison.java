7f539e3de2a605a528276314ba9f829fb151a8b5
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-03 10:43:32 +0900

    comparison

---
+++ src/chapter03/Comparison.java
@@ -0,0 +1,36 @@
+package chapter03;
+
+public class Comparison {
+	public static void main(String[] args) {
+		// == != > < >= <=
+		// 결과는 boolean 타입
+		
+		System.out.println(3 == 4);
+		System.out.println(3 == 3);
+		
+		System.out.println(3 != 5);
+		System.out.println(3 != 3);
+		
+		System.out.println(3 > 5);
+		System.out.println(5 > 3);
+		
+		System.out.println(3 < 5);
+		System.out.println(5 < 3);
+		
+		System.out.println(3 <= 5);
+		System.out.println(5 <= 3);
+		
+		System.out.println(3 >= 5);
+		System.out.println(5 >= 3);
+		
+	}
+}
+
+
+
+
+
+
+
+
+
