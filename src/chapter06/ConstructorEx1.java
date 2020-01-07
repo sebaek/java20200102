441fa612e3354b4a293995c373a6c786bd7727d0
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-07 16:17:56 +0900

    constructor

--- src/chapter06/ConstructorEx1.java
+++ src/chapter06/ConstructorEx1.java
@@ -5,23 +5,34 @@ public class ConstructorEx1 {
 	int intField2;
 	
 	public ConstructorEx1() {
-		intField1 = 33;
-		intField2 = 99;
-		System.out.println("인스턴스 초기화됨");
+//		intField1 = 33;
+//		intField2 = 99;
+//		System.out.println("인스턴스 초기화됨");
+		this(33, 99);
 	}
 	
 	public ConstructorEx1(int i) {
-		intField1 = i;
-		intField2 = 99;
+//		intField1 = i;
+//		intField2 = 99;
+//		System.out.println("인스턴스 초기화됨");
+		this(i, 99);
+	}
+	
+	public ConstructorEx1(int intField1, int intField2) {
+		this.intField1 = intField1;
+		this.intField2 = intField2;
+		System.out.println(this.intField1);
+		System.out.println(this.intField2);
 		System.out.println("인스턴스 초기화됨");
 	}
 	
-	public ConstructorEx1(int i, int j) {
-		intField1 = i;
-		intField2 = j;
-		System.out.println("인스턴스 초기화됨");
+	public ConstructorEx1(int j, String s) {
+		
 	}
 	
+	public ConstructorEx1(String s, int j) {
+		
+	}
 }
 
 
