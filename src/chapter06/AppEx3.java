1aad870fb7f7541f14422b7a7471496b66ab3ee2
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-08 09:31:26 +0900

    method, return

--- src/chapter06/AppEx3.java
+++ src/chapter06/AppEx3.java
@@ -2,6 +2,14 @@ package chapter06;
 
 public class AppEx3 {
 	public static void main(String[] args) {
+		MethodEx3 re1 = new MethodEx3();
+		int r = re1.method1();
+		double s = re1.method2();
+		
+		System.out.println(r);
+		
+		System.out.println("---------------");
+		
 		MethodEx2 me1 = new MethodEx2();
 		me1.method1();
 		me1.method2(9);
---
+++ src/chapter06/MethodEx3.java
@@ -0,0 +1,22 @@
+package chapter06;
+
+public class MethodEx3 {
+	int method1() {
+		System.out.println("메소드1 시작");
+		System.out.println("메소드1 끝.");
+		
+		return 9;
+	}
+	
+	double method2() {
+		System.out.println("메소드2 시작");
+		System.out.println("메소드2 끝.");
+		return 4;
+	}
+	
+	
+}
+
+
+
+
