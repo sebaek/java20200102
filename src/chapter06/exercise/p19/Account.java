d33fe0a889f3b0e2293b37c69f3aae881cca91d5
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-08 17:00:52 +0900

    exercise 19, 20

---
+++ src/chapter06/exercise/p19/Account.java
@@ -0,0 +1,19 @@
+package chapter06.exercise.p19;
+
+public class Account {
+	private static final int MIN_BALANCE = 0;
+	private static final int MAX_BALANCE = 1000000;
+	private int balance;
+	
+	public int getBalance() {
+		return this.balance;
+	}
+
+	public void setBalance(int balance) {
+		if (MIN_BALANCE <= balance && balance <= MAX_BALANCE) {
+			this.balance = balance;
+		}
+	}
+	
+	
+}
