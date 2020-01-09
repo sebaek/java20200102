373df6f3697bb444bb708950ed2b67e3bcf63c29
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-09 16:29:11 +0900

    polymorphism

--- src/chapter07/polymophism/AppEx1.java
+++ src/chapter07/polymophism/AppEx1.java
@@ -2,10 +2,27 @@ package chapter07.polymophism;
 
 public class AppEx1 {
 	public static void main(String[] args) {
-		Tiger tiger = new Tiger();
-		HouseCat cat = new HouseCat();
+		KindCat tiger = new Tiger();
+		KindCat cat = new HouseCat();
 		
 		tiger.cry();
 		cat.cry();
+		
+		Tiger t = (Tiger) tiger;
+		
+		System.out.println(cat instanceof Tiger);
+		System.out.println(cat instanceof HouseCat);
+		System.out.println(cat instanceof KindCat);
+		
+		if (cat instanceof Tiger) {
+			Tiger t2 = (Tiger) cat;
+		}
+		
+		t.cry();
 	}
 }
+
+
+
+
+
