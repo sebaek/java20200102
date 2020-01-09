3ffa2ccdded6585f1fc110974125aeaf927f9665
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-09 14:15:19 +0900

    inheritence constructor

--- src/chapter07/constructor/SubClass.java
+++ src/chapter07/constructor/SubClass.java
@@ -1,5 +1,14 @@
 package chapter07.constructor;
 
 public class SubClass extends SuperClass {
+	int k;
 
+	public SubClass(int i, int j, int k) {
+		super(i, j);
+		this.k = k;
+	}
+	
+	public SubClass(int i, int j) {
+		super(i, j);
+	}
 }
--- src/chapter07/constructor/SuperClass.java
+++ src/chapter07/constructor/SuperClass.java
@@ -1,5 +1,12 @@
 package chapter07.constructor;
 
 public class SuperClass {
+	int i;
+	int j;
+	
+	public SuperClass(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
 
 }
