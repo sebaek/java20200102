ab4e61acb07de37d2f8068e7b306842f252f1b35
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-10 11:47:11 +0900

    polymorphism, parameter type

---
+++ src/chapter07/polymorphism/section773/Bus.java
@@ -0,0 +1,8 @@
+package chapter07.polymorphism.section773;
+
+public class Bus extends Vehicle {
+	@Override
+	public void run() {
+		System.out.println("버스가 달립니다.");
+	}
+}
---
+++ src/chapter07/polymorphism/section773/Driver.java
@@ -0,0 +1,7 @@
+package chapter07.polymorphism.section773;
+
+public class Driver {
+	public void drive(Vehicle vehicle) {
+		vehicle.run();
+	}
+}
