fe3ed117de31fe262e20f01e99bf367db7a87900
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-13 09:42:26 +0900

    interface

---
+++ src/chapter08/inter/RemoteControlExample.java
@@ -0,0 +1,14 @@
+package chapter08.inter;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		RemoteControl rc = null;
+		rc = new Television();
+		rc.turnOn();
+		rc.turnOff();
+		
+		rc = new Audio();
+		rc.turnOn();
+		rc.turnOff();
+	}
+}
