b7d59e2cb3fc57dbcfb6e4c73a579f9ea843e3b0
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-14 12:27:25 +0900

    catch order

--- src/chapter10/CatchOrderExample.java
+++ src/chapter10/CatchOrderExample.java
@@ -1,6 +1,6 @@
 package chapter10;
 
-public class CatchByExceptionKindExample {
+public class CatchOrderExample {
 	public static void main(String[] args) {
 		try {
 			String data1 = args[0];
@@ -10,15 +10,14 @@ public class CatchByExceptionKindExample {
 			int result = value1 + value2;
 			System.out.println(data1 + " + " + data2 + " = " + result);
 		} catch (ArrayIndexOutOfBoundsException e) {
-//			e.printStackTrace();
 			System.out.println("실행 매개값의 수가 부족합니다.");
-			System.out.println("[실행 방법]");
-			System.out.print("java CatchByExceptionKindExample ");
-			System.out.println("값1 값2");
 		} catch (Exception e) {
-			System.out.println("숫자로 변환할 수 없습니다.");
+			System.out.println("실행에 문제가 있습니다.");
 		} finally {
 			System.out.println("다시 실행하세요.");
 		}
+		
+		
+		
 	}
 }
