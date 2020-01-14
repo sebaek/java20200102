c2569a8a6c68413507ba70e2e1ed31bb56ea9891
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-14 10:31:17 +0900

    try catch finally

--- src/chapter10/TryCatchFinallyExample.java
+++ src/chapter10/TryCatchFinallyExample.java
@@ -3,9 +3,15 @@ package chapter10;
 public class TryCatchFinallyExample {
 	public static void main(String[] args) {
 		try {
+			System.out.println("try 시작");
 			Class clazz = Class.forName("java.lang.String2");
+			System.out.println("try 종료");
 		} catch (ClassNotFoundException e) {
+			System.out.println("catch 시작");
 			System.out.println("exception 발생");
+			System.out.println("catch 종료");
 		}
+		
+		System.out.println("실행을 이어감.");
 	}
 }
