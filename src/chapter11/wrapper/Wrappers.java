143f02cf3c6f930af16923b163f0561a68dd9f2e
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-15 10:05:51 +0900

    Wrapper

---
+++ src/chapter11/wrapper/Wrappers.java
@@ -0,0 +1,48 @@
+package chapter11.wrapper;
+
+public class Wrappers {
+	public static void main(String[] args) {
+		byte b1 = 23;
+		Byte b2 = new Byte(b1);
+		
+		short s1 = 234;
+		Short s2 = new Short(s1);
+		
+		int i1 = 98765;
+		Integer i2 = new Integer(i1);
+		
+		long l1 = 987611234;
+		Long l2 = new Long(l1);
+		
+		float f1 = 3.14F;
+		Float f2 = new Float(f1);
+		
+		double d1 = 3.14;
+		Double d2 = new Double(d1);
+		
+		boolean bn1 = false;
+		Boolean bn2 = new Boolean(bn1);
+		
+		char c1 = 'a';
+		Character c2 = new Character(c1);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
