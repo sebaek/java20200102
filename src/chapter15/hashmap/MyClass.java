0f9a7b63398497cd2585e96a206b4bafdc6ff068
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-16 14:37:06 +0900

    hashmap

---
+++ src/chapter15/hashmap/MyClass.java
@@ -0,0 +1,12 @@
+package chapter15.hashmap;
+
+public class MyClass {
+	static class NestedClass {
+		
+	}
+	
+	public static void main(String[] args) {
+		MyClass.NestedClass n = new MyClass.NestedClass();
+		
+	}
+}
