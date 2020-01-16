d4d4d500dbb02e363965006b5ffe3f871eb6f847
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-16 10:05:15 +0900

    hashset lotto

--- src/chapter15/hashset/HashSetEx3.java
+++ src/chapter15/hashset/HashSetEx3.java
@@ -9,53 +9,44 @@ public class HashSetEx3 {
 		Set<Integer> mylotto = new HashSet<>();
 		Set<Integer> computer = new HashSet<>();
 		Random random = new Random();
-		
+
 		while (mylotto.size() < 6) {
 			mylotto.add(random.nextInt(45) + 1);
 		}
-		
+
 		System.out.println("내꺼:" + mylotto);
-		
-		while (computer.size() < 6) {
-			computer.add(random.nextInt(45) + 1);
-		}
-		
-		System.out.println("방송:" + computer);
-		
-		boolean won = true;
-		
-		for (Integer c : computer) {
+
+		long count = 0;
+		while (true) {
+			count++;
+			while (computer.size() < 6) {
+				computer.add(random.nextInt(45) + 1);
+			}
+
+//			System.out.println("방송:" + computer);
+
+			boolean won = true;
+
+			for (Integer c : computer) {
 //			System.out.println(c);
 //			System.out.println(mylotto.contains(c));
-			if (!mylotto.contains(c)) {
-				won = false;
-				break;
+				if (!mylotto.contains(c)) {
+					won = false;
+					break;
+				}
 			}
-		}
-		
-		if (won) {
-			System.out.println("1등입니다.");
-		} else {
-			System.out.println("1등이 아닙니다.");
+
+			if (won) {
+				System.out.println("방송:" + computer);
+				System.out.println("1등입니다.");
+				System.out.println(count + "회에 당첨");
+				System.out.println((count/52) + "년 후 당첨");
+				break;
+			} else {
+//				System.out.println("1등이 아닙니다.");
+				computer.clear();
+			}
+
 		}
 	}
 }
-	
-	
-	
-	
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
