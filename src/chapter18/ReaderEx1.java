ba61c09e6bea89e1d6865dcfd03e89896c2af0df
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-17 10:26:34 +0900

    file reader

---
+++ src/chapter18/ReaderEx1.java
@@ -0,0 +1,36 @@
+package chapter18;
+
+import java.io.FileReader;
+import java.io.Reader;
+
+public class ReaderEx1 {
+	public static void main(String[] args) throws Exception {
+		Reader fr = new FileReader("lionking.txt");
+		
+		char c = (char) fr.read();
+		System.out.println(c);
+		
+		c = (char) fr.read();
+		System.out.println(c);
+		
+		c = (char) fr.read();
+		System.out.println(c);
+
+		int i;
+		while ((i = fr.read()) != -1) {
+			c = (char) i;
+			System.out.println(c);
+		}
+		
+		
+		
+		fr.close();
+	}
+}
+
+
+
+
+
+
+
