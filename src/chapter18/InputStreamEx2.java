ee13c0bbafb94cae05c642a48411c010f969baeb
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-17 12:11:35 +0900

    file input stream

--- src/chapter18/InputStreamEx2.java
+++ src/chapter18/InputStreamEx2.java
@@ -7,14 +7,22 @@ import java.io.InputStream;
 public class InputStreamEx2 {
 	public static void main(String[] args) {
 		try (InputStream is = new FileInputStream("다형성1.png");) {
+			byte[] byteArr = new byte[10000];
 			
 			int cnt = 0;
 			
-			while (is.read() != -1) {
-				cnt++;
-			}
+			cnt = is.read(byteArr);
+			System.out.println(cnt);
+			cnt = is.read(byteArr);
+			System.out.println(cnt);
+			cnt = is.read(byteArr);
+			System.out.println(cnt);
 			
-			System.out.println(cnt + "bytes 크기의 파일");
+//			while (is.read() != -1) {
+//				cnt++;
+//			}
+//			
+//			System.out.println(cnt + "bytes 크기의 파일");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
