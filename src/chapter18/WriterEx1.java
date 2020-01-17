add8acac53164da39b700a84e1974b2510c615a1
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-17 11:16:57 +0900

    file writer

--- src/chapter18/WriterEx1.java
+++ src/chapter18/WriterEx1.java
@@ -1,21 +1,41 @@
 package chapter18;
 
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.Reader;
 import java.io.Writer;
 
 public class WriterEx1 {
 	public static void main(String[] args) throws Exception {
 		Writer fw = new FileWriter("output.txt");
+		Reader fr = new FileReader("output.txt");
 		
 		fw.write('한');
 		fw.write('a');
 		fw.write(97);
 		fw.write(new char[] {'한', '글', '\n', 'a', 'b', 'c'});
 		fw.write("한글abcde");
-		
+
 		fw.close();
+		
+		// 
+		int cnt = 0;
+		while (fr.read() != -1) {
+			cnt++;
+		}
+		System.out.println(cnt + "개의 문자");
+		
+		fr.close();
 	}
 }
+
+
+
+
+
+
+
+
 
 
 
