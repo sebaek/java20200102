1a799b67452419f28338c0794f3963edf6107fad
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-20 09:34:58 +0900

    bufferedinputStream

--- src/chapter18/buffered/BufferdInputStreamEx1.java
+++ src/chapter18/buffered/BufferdInputStreamEx1.java
@@ -4,7 +4,28 @@ import java.io.FileInputStream;
 
 public class BufferdInputStreamEx1 {
 	public static void main(String[] args) throws Exception {
-		FileInputStream fis = new FileInputStream("다형성1.png");
+		inputstream();
+		bufferedinputstream();
+	}
+	public static void bufferedinputstream() throws Exception {
+		FileInputStream fis = new FileInputStream("out.txt");
+
+		long start = System.currentTimeMillis();
+		while (fis.read() != -1) {
+			
+		}
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println((end-start) + "밀리초");
+		
+		System.out.println("읽기 완료.");
+		
+		fis.close();
+	}
+	
+	public static void inputstream() throws Exception {
+		FileInputStream fis = new FileInputStream("out.txt");
 
 		long start = System.currentTimeMillis();
 		while (fis.read() != -1) {
